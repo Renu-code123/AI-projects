3c97f4fac0f2afaa7e33876486dbd6df831929d8
RENU KUMARI PRAJAPATI <152794326+Renu-code123@users.noreply.github.com>
2026-06-21 23:13:29 +0530

    Create app.py

---
+++ Sentiment Analysis with an Recurrent Neural Networks (RNN)/ML model /app.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import re  
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import SimpleRNN, Dense, Embedding
