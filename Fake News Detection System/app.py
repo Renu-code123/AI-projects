d6777d2841ffd512e95b64404c52607690902c4b
RENU KUMARI PRAJAPATI <152794326+Renu-code123@users.noreply.github.com>
2026-06-28 22:34:56 +0530

    Remove model architecture from app.py
    Removed model architecture and compilation code from app.py.

--- Fake News Detection System/app.py
+++ Fake News Detection System/app.py
@@ -60,15 +60,3 @@ test_sequences1 = np.array(test_sequences1)
 !unzip glove.6B.zip
 
 #10. Model Architecture
-model = tf.keras.Sequential([
-    tf.keras.layers.Embedding(vocab_size1 + 1, embedding_dim, input_length=max_length, 
-                              weights=[embedding_matrix], trainable=False),
-    tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.Conv1D(64, 5, activation='relu'),
-    tf.keras.layers.MaxPooling1D(pool_size=4),
-    tf.keras.layers.LSTM(64),
-    tf.keras.layers.Dense(1, activation='sigmoid')
-])
-
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-model.summary()
